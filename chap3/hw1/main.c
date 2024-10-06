4d6af7f9e34a38b194fe105db8a060967afa3752
dbtmdalsfk <lan04101@naver.com>
2024-10-06 06:10:37 +0000

    132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132132

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-#include "copy.h"
+#include <string.h>  
+#define MAXLINE 100 
 
-char line[MAXLINE];
-char longest[MAXLINE]; 
+char lines[5][MAXLINE];  
+char temp[MAXLINE];      
 
-main(){
-  int len;
-  int max;
-  max = 0;
+int main(){
+    int len;
+    int i, j;
+
+    for(i = 0; i < 5; i++){
+        fgets(lines[i], MAXLINE, stdin);}
+
   
-  for (i = 0; i < 5; i++){
-  while(gets(line) != NULL) {
-    len = strlen(line);
-  }
-  
-  for(i =1; i <= 4; i++){
-	  for(j = 0; j<5-i; j++){
-    if(len > max) {
-       max = len;
-       copy(line, longest);
-      }
+    for(i = 0; i < 4; i++){
+        for(j = i + 1; j < 5; j++){
+            if(strlen(lines[i]) < strlen(lines[j])){ 
+                strcpy(temp, lines[i]);              
+                strcpy(lines[i], lines[j]);
+                strcpy(lines[j], temp);
+            }
+        }
     }
-  }
-  for(i=0; i<5; i++){
-  if(max > 0)
-    printf("%s \n", longest);
-  }
-  return 0;
+
+
+    for(i = 0; i < 5; i++){
+        printf("%s", lines[i]);
+    }
+
+    return 0;
 }
